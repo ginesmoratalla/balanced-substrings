3363cf83609664128d99609d25b14f3ff9d85b8a
Gines Moratalla <ginesmoratalla@gmail.com>
2024-03-24 23:50:40 +0100

    hopefully nano fixes indentation comment issues?

--- src/cw.js
+++ src/cw.js
@@ -4,14 +4,14 @@
  * 
  */
 "use strict";
-const cwlib = require( './cwlib.js' );										                                // Import 'console.warn()lib.js' file
-cwlib.setup(process.argv[2]);															                                // Setup function from 'cwlib.js' --> reads input file from terminal
+const cwlib = require( './cwlib.js' );										// Import 'console.warn()lib.js' file
+cwlib.setup(process.argv[2]);							                                // Setup function from 'cwlib.js' --> reads input file from terminal
 
-let data_string = '';																	                                    // String used to check for balanced substrings
-let line_index = 0;																		                                    // Variable that tracks the current line index
-let total_max = 0;																		                                    // Variable that tracks the length of the current longest balanced substring
-let line_longest_substr = [];															                                // Array that will store the longest substrings from one line
-let total_longest_substr = [];														                                // Array that will store longest substrings from all the lines
+let data_string = '';											        // String used to check for balanced substrings
+let line_index = 0;											        // Variable that tracks the current line index
+let total_max = 0;												// Variable that tracks the length of the current longest balanced substring
+let line_longest_substr = [];											// Array that will store the longest substrings from one line
+let total_longest_substr = [];											// Array that will store longest substrings from all the lines
 /**
  * @function balancedSubstring below checks wether a substring 'substring' is balanced
  * @param {*} substring passed substring from 'data' hook
@@ -25,7 +25,7 @@ function balancedSubstring(substring) {
 	
 	let unique_chars = [...new Set(substring)];	   									
 
-	return unique_chars.every((char) => {												                            // every() === true if all characters 'char' in 'unique_chars' meet the conditions below
+	return unique_chars.every((char) => {									// every() === true if all characters 'char' in 'unique_chars' meet the conditions below
 	  let lowerCase = substring.filter((i) => i === char.toLowerCase()).length;			        // lowerCase --> counter for lowercase characters
 	  let upperCase = substring.filter((i) => i === char.toUpperCase()).length;			        // upperCase --> counter for uppercase characters
 	  return lowerCase === upperCase; 
@@ -43,19 +43,19 @@ function balancedSubstring(substring) {
  */
 function currentSubstrings(str) {
 
-	let character_array = [...str];														                              // Convert passed string into array of characters
+	let character_array = [...str];										// Convert passed string into array of characters
 
-	character_array.map((current_char, i) =>											                          // 'current_char' is unused, as we only need index of chars ('i' and 'j')
-		character_array.slice(i).map((current_char, j) => {								                    // Starting in index i, for all j, find all substrings in range [i, j]
+	character_array.map((current_char, i) =>								// 'current_char' is unused, as we only need index of chars ('i' and 'j')
+		character_array.slice(i).map((current_char, j) => {						// Starting in index i, for all j, find all substrings in range [i, j]
 
-			let sub_array = character_array.slice(i, i + j + 1);						                    // Get substring starting from i and ending at index i + j										
+			let sub_array = character_array.slice(i, i + j + 1);				        // Get substring starting from i and ending at index i + j										
 
-			if(balancedSubstring(sub_array) && 											                            // True IF sub_array is balanced and it was not previously added
+			if(balancedSubstring(sub_array) && 							// True IF sub_array is balanced and it was not previously added
 			!line_longest_substr.some(index => index[0] === i && index[1] === i + j)) {	        // to avoid adding repeated strings by index
 
-				let substring_index = [i, i + j];										                              // Create setup (this variable will be added to the list of balanced substrings)
-				console.log(substring_index);											                                // Print current balanced substring
-				line_longest_substr.push(substring_index);								                        // Add substring to current line's set of balanced substrings
+				let substring_index = [i, i + j];						// Create setup (this variable will be added to the list of balanced substrings)
+				console.log(substring_index);							// Print current balanced substring
+				line_longest_substr.push(substring_index);					// Add substring to current line's set of balanced substrings
 			}
     	})
     );
@@ -72,25 +72,25 @@ cwlib.on( 'ready', function() { cwlib.run(); });
  * 'data' variable is treated as a character (each char from input file)
  */
 cwlib.on( 'data', function( data ) {												
-	data_string += data;																                                    // Push data character into a data_string (Type String)
-	currentSubstrings(data_string);														                              // Send current data string to 'currentSubstrings' function (creates all possible substrings)
+	data_string += data;									                 // Push data character into a data_string (Type String)
+	currentSubstrings(data_string);		            				                         // Send current data string to 'currentSubstrings' function (creates all possible substrings)
 } );
 /**
  * Reset hook --> function called when the end of a line is detected in any input file given
  */
 cwlib.on( 'reset', function() {
 
-	const line_max = Math.max(...line_longest_substr.map(arr => arr[1] - arr[0] + 1));	    // Check for current line's max substring length (by substracting index bounds)
-	if(line_max > total_max) total_max = line_max;										                      // Update global max length if this line's length is bigger
+	const line_max = Math.max(...line_longest_substr.map(arr => arr[1] - arr[0] + 1));	    		  // Check for current line's max substring length (by substracting index bounds)
+	if(line_max > total_max) total_max = line_max;								  // Update global max length if this line's length is bigger
 
-	const result = line_longest_substr.filter(subarray =>								                    // Filter out all balanced substrings that are not of line_max length
+	const result = line_longest_substr.filter(subarray =>						          // Filter out all balanced substrings that are not of line_max length
 		subarray[1] - subarray[0] + 1 === line_max);
 
 	console.log("Longest balanced substrings for this line: ", result, "\n");			          // Print statement --> prints current line's substring index bounds for all strings with length 'line_max'
-	total_longest_substr.push([line_index, result]);									                      // Push all max length substrings from this line to overall max substring array (with their line index)
-	line_longest_substr = [];															                                  // Empty current line's substrings to work with next lines
-	line_index++;																		                                        // Increment the index line for the next line in the input file
-	data_string = '';																	                                      // Empty data_string to run functions again for any new line
+	total_longest_substr.push([line_index, result]);							  // Push all max length substrings from this line to overall max substring array (with their line index)
+	line_longest_substr = [];								                  // Empty current line's substrings to work with next lines
+	line_index++;											          // Increment the index line for the next line in the input file
+	data_string = '';										          // Empty data_string to run functions again for any new line
 } );
 /**
  * End hook --> function called when input file ends, running process will stop after this function finished
@@ -98,9 +98,9 @@ cwlib.on( 'reset', function() {
  */
 cwlib.on( 'end', function() {
 
-	const filtered_substrings = total_longest_substr.filter(substring =>				            // substring[0] will correspond to the line index
-	substring[1].some(index => index[1] - index[0] + 1 === total_max)					              // substring[1] contains max length substrings for each line
-	);																					                                            // some() will retrieve only the ones where the index substraction === total_max
+	const filtered_substrings = total_longest_substr.filter(substring =>				          // substring[0] will correspond to the line index
+	substring[1].some(index => index[1] - index[0] + 1 === total_max)					  // substring[1] contains max length substrings for each line
+	);											                  // some() will retrieve only the ones where the index substraction === total_max
 	console.log("Overall longest balanced substrings:"); 
-	filtered_substrings.forEach((substring) => {console.log(substring)});				            // Print all balanced substrings of total_max length with their respective index line
+	filtered_substrings.forEach((substring) => {console.log(substring)});				          // Print all balanced substrings of total_max length with their respective index line
 }); 
